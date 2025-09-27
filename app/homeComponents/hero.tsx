5dec8870dd2ba532c51880525795d60ded032d26
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-09-27 19:51:18 +0500

    feat: hero section

---
+++ app/homeComponents/hero.tsx
@@ -0,0 +1,21 @@
+
+
+const Hero = () => {
+    return (
+        <div className="flex flex-row w/full  ">
+            <span className="flex flex-col md:w-6/10  text-[1.125rem]  ">
+                <span className="text-wrap">
+                    Writing full-stack code with a backend-first mindset — aiming to understand what makes systems reliable, scalable, and fast.<br/>
+                    What I Actually Do <br/> 
+                </span>
+                <span className="ml-[1.125rem] text-wrap ">
+                    • Build full-stack applications with Next.js, Express, SQL and more <br/>
+                    • Explore backend systems, databases, auth flows, and APIs<br/>
+                    • Diving deeper into infra, backend patterns, and how everything fits together
+                </span>
+            </span>
+        </div>
+    )
+}
+
+export default Hero
